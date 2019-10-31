bca6b5829a8c03dc373fc97519109665884889eb
meksconways <muhammedeminkisi@gmail.com>
2019-10-31 12:24:53 +0300

    >Borunun ekonomik capi activity eklendi

---
+++ app/src/main/java/com/nuriefeoglu/mbuproject/borununekonomikcapi/BorununEkonomikCapiActivity.java
@@ -0,0 +1,16 @@
+package com.nuriefeoglu.mbuproject.borununekonomikcapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nuriefeoglu.mbuproject.R;
+
+public class BorununEkonomikCapiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_borunun_ekonomik_capi);
+    }
+}
